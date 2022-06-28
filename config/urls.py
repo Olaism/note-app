856e50a674d91e7db92c96624ae892f913bd7d4e
olaism <olaism01@gmail.com>
2022-06-28 14:05:08 -0700

    Added all password reset functionalities

--- config/urls.py
+++ config/urls.py
@@ -15,4 +15,12 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('settings/password/change/', views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
     path('settings/password/change/done/', views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+    path('password/reset/', views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+        ), name='password_reset'),
+    path('password/reset/done/', views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password/reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password/reset/success/', views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete')
 ]
