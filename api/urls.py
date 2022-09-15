fb8338cc31d466740ba0cedf930331773b519dae
olaism <olaism01@gmail.com>
2022-09-15 13:51:09 -0700

    Added notes endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,9 @@ from .views import (
     APIHome,
     NoteList,
     NoteDetail,
+    NoteCreate,
+    NoteUpdate,
+    NoteDelete,
 )
 
 app_name = 'api'
@@ -12,6 +15,9 @@ urlpatterns = [
     path('notes/', NoteList.as_view()),
     path('home/', APIHome.as_view(), name='home'),
     path('notes/<uuid:pk>/', NoteDetail.as_view()),
+    path('notes/create/', NoteCreate.as_view()),
+    path('notes/<uuid:pk>/edit/', NoteUpdate.as_view()),
+    path('notes/<uuid:pk>/delete/', NoteDelete.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -9,17 +9,39 @@ class APIHome(TemplateView):
     template_name = 'api/home.html'
 
 
-class NoteList(generics.ListCreateAPIView):
+class NoteList(generics.ListAPIView):
     serializer_class = NoteSerializer
 
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(created_by=user)
 
+class NoteDetail(generics.RetrieveAPIView):
+    permission_classes = (IsNoteCreator,)
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(created_by=user)
+
+class NoteCreate(generics.CreateAPIView):
+    serializer_class = NoteSerializer
+
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+class NoteUpdate(generics.UpdateAPIView):
     permission_classes = (IsNoteCreator,)
-    queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(created_by=user)
+
+class NoteDelete(generics.DestroyAPIView):
+    permission_classes = (IsNoteCreator,)
+    serializer_class = NoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(created_by=user)
